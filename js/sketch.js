5cce7b31c43cae0fbff9de5dabcaa36d3ace1b99
MarkBWilliams <mark.b.will1@gmail.com>
2021-04-07 19:15:44 -0700

    worked out Clear enitre grid funtion

--- js/sketch.js
+++ js/sketch.js
@@ -28,41 +28,41 @@ function makeCols(colNum) {
   }
 }
 /* function to change color og grid */
+const changeColorBtn = document.createElement("button");
+changeColorBtn.textContent = "Change Color";
+gridContainer.appendChild(changeColorBtn);
+changeColorBtn.addEventListener("click", changeColor);
 function changeColor() {
-  const changeColorBtn = document.createElement("button");
-  changeColorBtn.textContent = "Change Color";
-  gridContainer.appendChild(changeColorBtn);
-  changeColorBtn.addEventListener("click", () => {
-    for (i = 0; i < col.length; i++) {
-      col[i].addEventListener("mouseover", (e) => {
-        e.target.style.background = "red";
-      });
-    }
-  });
+  for (i = 0; i < col.length; i++) {
+    col[i].addEventListener("mouseover", (e) => {
+      e.target.style.background = "red";
+    });
+  }
 }
 
+/* button for resetColor function */
+const resetColorBtn = document.createElement("button");
+resetColorBtn.textContent = "reset color";
+gridContainer.append(resetColorBtn);
+resetColorBtn.addEventListener("click", resetColor);
+//resetColor function
 function resetColor() {
-  const resetColorBtn = document.createElement("button");
-  resetColorBtn.textContent = "reset color";
-  gridContainer.append(resetColorBtn);
-  resetColorBtn.addEventListener("click", () => {
-    cols = document.querySelectorAll(".colCell");
-    for (i = 0; i < cols.length; i++) {
-      cols[i].style.background = "";
-    }
-  });
+  cols = document.querySelectorAll(".colCell");
+  for (i = 0; i < cols.length; i++) {
+    cols[i].style.background = "";
+  }
 }
 /* function to get input for the output of the the number of grids */
-//button
+//button for promt question
 const createGridbtn = document.createElement("button");
-createGridbtn.textContent = `Grid Size ?`;
+createGridbtn.textContent = `grid size ?`;
 gridContainer.appendChild(createGridbtn);
+createGridbtn.addEventListener("click", createGrid);
 
 /* function to create Grid */
-createGridbtn.addEventListener("click", createGrid);
 function createGrid() {
-  userChoice = parseInt(window.prompt("Grid size?"));
-  if (userChoice > 16) return;
+  userChoice = parseInt(window.prompt("Choose grid size between 1-16"));
+  if (userChoice > 64) return;
   makeRows(userChoice);
   makeCols(userChoice);
 }
@@ -73,10 +73,16 @@ clrBtn.textContent = "Clear the grid";
 gridContainer.appendChild(clrBtn);
 clrBtn.addEventListener("click", clearGrid);
 function clearGrid() {
-  makeCols();
-  makeRows();
+  colm = document.querySelectorAll(".colCell");
+  rows = document.querySelectorAll(".rowCell");
+  for (i = 0; i < colm.length; i++) {
+    colm[i].remove();
+  }
+  for (i = 0; i < rows.length; i++) {
+    rows[i].remove();
+  }
 }
 //createGrid();
-changeColor();
-resetColor();
+//changeColor();
+//resetColor();
 //clearGrid();
